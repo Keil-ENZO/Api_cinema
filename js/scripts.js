6194fb8d44b3300625a1a62724fbe09ae04651fc
Keil-ENZO <enzo.keil06@icloud.com>
2023-06-25 21:37:08 +0200

    menu burger

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,30 @@ const url =
   apiKey +
   "&language=fr-FR&page=1&sort_by=popularity.desc";
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
-    document.getElementById("title").innerHTML = data.results[0].title;
+// fetch(url)
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log(data);
+//     document.getElementById("title").innerHTML = data.results[0].title;
 
-    //poster
-    document.getElementById("poster").src =
-      "https://image.tmdb.org/t/p/w500" + data.results[0].poster_path;
+//     //poster
+//     document.getElementById("poster").src =
+//       "https://image.tmdb.org/t/p/w500" + data.results[0].poster_path;
 
-    // //image
-    // document.getElementById("video").src =
-    //   "https://image.tmdb.org/t/p/w500" + data.backdrop_path;
-  });
+//     // //image
+//     // document.getElementById("video").src =
+//     //   "https://image.tmdb.org/t/p/w500" + data.backdrop_path;
+//   });
+
+function Menu(e) {
+  let menu = document.querySelector("ul");
+  e.classList.contains("fa-bars")
+    ? (e.classList.replace("fa-bars", "fa-xmark"),
+      menu.classList.add("top-[80px]"),
+      menu.classList.add("opacity-100"))
+    : (e.classList.replace("fa-xmark", "fa-bars"),
+      menu.classList.remove("top-[80px]"),
+      menu.classList.remove("opacity-100"));
+}
+
+
