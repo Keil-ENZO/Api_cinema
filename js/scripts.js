a91b9e670c7ce83a03d84f05b24fb1027a010e42
Keil-ENZO <enzo.keil06@icloud.com>
2023-06-29 22:24:08 +0200

    trailer au hover

--- js/scripts.js
+++ js/scripts.js
@@ -4,115 +4,128 @@ const url =
   "https://api.themoviedb.org/3/discover/movie?api_key=" +
   apiKey +
   `&language=fr-FR&page=${page}&sort_by=popularity.desc`;
+  const cardHoverState = {};
 
-fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log(data);
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
 
-    let count = 0;
-    let cardDisplay = 0;
-    let moviesId = data.results.map((movie) => movie.id);
-
-    for (let i = 0; i < data.results.length; i++) {
+      let count = 0;
+      let cardDisplay = 0;
+      let moviesId = data.results.map((movie) => movie.id);
       const screenWidth = window.innerWidth;
+
       console.log(screenWidth);
 
-      if (screenWidth < 460) {
-        cardDisplay = 1;
-      } else if (screenWidth < 700) {
-        cardDisplay = 2;
-      } else if (screenWidth < 950) {
-        cardDisplay = 3;
-      } else if (screenWidth < 1200) {
-        cardDisplay = 4;
-      } else {
-        cardDisplay = 5;
-      }
-
-      if (count < cardDisplay) {
-        const info = data.results[i];
-
-        const card = document.createElement("div");
-        card.classList.add("card");
-
-        const poster = document.createElement("img");
-        poster.classList.add("poster");
-        poster.setAttribute("id", "poster");
-        poster.src = "https://image.tmdb.org/t/p/w500" + info.poster_path;
-
-        const note = document.createElement("p");
-        note.classList.add("note");
-        note.setAttribute("id", "note");
-        note.innerHTML = info.vote_average * 10 + "%";
-
-        if (info.vote_average * 10 <= 70 && info.vote_average * 10 >= 40) {
-          note.classList.add("bg-yellow-500");
-        } else if (info.vote_average * 10 < 40) {
-          note.classList.add("bg-red-500");
+      for (let i = 0; i < data.results.length; i++) {
+        if (screenWidth < 460) {
+          cardDisplay = 1;
+        } else if (screenWidth < 700) {
+          cardDisplay = 2;
+        } else if (screenWidth < 950) {
+          cardDisplay = 3;
+        } else if (screenWidth < 1200) {
+          cardDisplay = 4;
         } else {
-          note.classList.add("bg-green-500");
+          cardDisplay = 5;
         }
 
-        const title = document.createElement("h2");
-        title.classList.add("title");
-        title.setAttribute("id", "title");
-        if (info.title.length > 20) {
-          title.innerHTML = info.title.substring(0, 20) + "...";
-        } else {
-          title.innerHTML = info.title;
-        }
+        if (count < cardDisplay) {
+          const info = data.results[i];
 
-        const date = document.createElement("p");
-        date.classList.add("title");
-        date.setAttribute("id", "date");
-        date.innerHTML = formatDate(info.release_date);
+          const card = document.createElement("div");
+          card.classList.add("card");
+          card.setAttribute("id", moviesId[i]);
 
-        card.appendChild(poster);
-        card.append(note);
-        card.appendChild(title);
-        card.appendChild(date);
-        best.appendChild(card);
+          const poster = document.createElement("img");
+          poster.classList.add("poster");
+          poster.setAttribute("id", "poster");
+          poster.src = "https://image.tmdb.org/t/p/w500" + info.poster_path;
 
-        count++;
+          const note = document.createElement("p");
+          note.classList.add("note");
+          note.setAttribute("id", "note");
+          note.innerHTML = info.vote_average * 10 + "%";
 
-        card.addEventListener("mouseover", () => {
-          let movieId = moviesId[i];
-          console.log(movieId);
-          fetch(
-            `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${apiKey}`
-          ).then((response) => {
-            response.json().then((data) => {
-              console.log(data);
+          if (info.vote_average * 10 <= 70 && info.vote_average * 10 >= 40) {
+            note.classList.add("bg-yellow-500");
+          } else if (info.vote_average * 10 < 40) {
+            note.classList.add("bg-red-500");
+          } else {
+            note.classList.add("bg-green-500");
+          }
 
-              const video = data.results[0].key;
+          const title = document.createElement("h2");
+          title.classList.add("title");
+          title.setAttribute("id", "title");
+          if (info.title.length > 20) {
+            title.innerHTML = info.title.substring(0, 20) + "...";
+          } else {
+            title.innerHTML = info.title;
+          }
 
-              const iframe = document.createElement("iframe");
-              iframe.setAttribute(
-                "src",
-                `https://www.youtube.com/embed/${video}`
-              );
-              poster.replaceWith(iframe);
-              iframe.setAttribute("width", "100%");
-              iframe.setAttribute("height", "100%");
-              iframe.setAttribute("frameborder", "0");
-              iframe.setAttribute("allowfullscreen", "true");
+          const date = document.createElement("p");
+          date.classList.add("title");
+          date.setAttribute("id", "date");
+          date.innerHTML = formatDate(info.release_date);
 
-              card.appendChild(iframe);
-            });
+          card.appendChild(poster);
+          card.append(note);
+          card.appendChild(title);
+          card.appendChild(date);
+          best.appendChild(card);
+
+          count++;
+
+          card.addEventListener("mouseover", () => {
+            const cardId = card.getAttribute("id");
+            trailer(cardId);
           });
 
-          console.log("mouse hover");
-        });
-
-        card.addEventListener("mouseout", () => {
-          card.classList.remove("transform", "scale-105");
-
-          console.log("mopuse out");
-        });
+          card.addEventListener("mouseout", () => {
+            const iframe = document.querySelector("iframe");
+            iframe.replaceWith(poster);
+          });
+        }
       }
+    });
+
+  function trailer(id) {
+    const card = document.getElementById(id);
+    const poster = card.querySelector(".poster");
+
+    // Vérifier si le trailer a déjà été chargé pour cette carte
+    if (cardHoverState[id]) {
+      return; // Sortir de la fonction si le trailer a déjà été chargé
     }
-  });
+
+    fetch(`https://api.themoviedb.org/3/movie/${id}/videos?api_key=${apiKey}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+
+        const video = data.results[0].key;
+
+        const iframe = document.createElement("iframe");
+        iframe.setAttribute("src", `https://www.youtube.com/embed/${video}`);
+        iframe.setAttribute("width", "100%");
+        iframe.setAttribute("height", "100%");
+        iframe.setAttribute("frameborder", "0");
+        iframe.setAttribute("allowfullscreen", "true");
+        iframe.setAttribute("allow", "autoplay; encrypted-media");
+
+        // Supprimer l'élément poster et le remplacer par l'iframe
+        if (poster && poster.parentNode) {
+          poster.parentNode.replaceChild(iframe, poster);
+        }
+
+        cardHoverState[id] = true; // Définir l'indicateur pour indiquer que le trailer a été chargé
+      });
+  }
+  
+  
+  
 
 function formatDate(date) {
   const newDate = new Date(date);
